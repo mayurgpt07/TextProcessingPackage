8a4e55724d010ab16b6cf7f3a490ec5cf00b2b7a
mayurgpt07 <mayurgpt07@gmail.com>
2021-05-02 17:58:11 -0700

    Installation setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TextProcessingPackage',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='mayurgpt07@gmail.com',
+license='MIT',
+packages=['TextProcessingPackage'],
+zip_safe=False)
