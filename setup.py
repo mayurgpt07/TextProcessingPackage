0da3366ad594b95741c05ebaeb94339bb1932761
mayurgpt07 <mayurgpt07@gmail.com>
2021-05-16 12:57:08 -0700

    Minor changes in version

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='TextPreProcessing',
-version='0.2',
+setup(name='TextPreProc',
+version='0.4',
 description='Text preprocessing made easy in python',
 url='https://github.com/mayurgpt07/TextProcessingPackage/',
 author='auth',
 author_email='mayurgpt07@gmail.com',
 license='MIT',
 packages=find_packages(),
+install_requires = [
+    'pandas',
+    'nltk',
+    'numpy',
+    'matplotlib',
+    'wordcloud',
+    'beautifulsoup4'
+],
 zip_safe=False)
