d394cfaf1a26d1a599b8eedb8399082e23697c40
mayurgpt07 <mayurgpt07@gmail.com>
2021-05-23 17:29:19 -0700

    Version 1 of Topic modelling

--- TopicModelling.py
+++ TopicModelling.py
@@ -6,25 +6,38 @@ from gensim.corpora import Dictionary
 from gensim.corpora import MmCorpus
 from gensim import corpora, models
 from gensim.models.phrases import Phrases, Phraser
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
 class Topic_Modelling:
 
-    def __init__(self, data, column_name, vectorizer_type, required_parameters, show_visualization = True, save_fig = False, is_sklearn = True):
+    def __init__(self, data, column_name, vectorizer_type = 'bow', topic_modelling_type = 'lda', vectorizer_parameters = {}, topic_model_parameters = {}, num_topics = 10, show_visualization = True, save_fig = False, fig_title = 'Topics in the data', is_sklearn = True):
         self.data = data
         self.column_name = column_name
         self.type = vectorizer_type
+        self.topic_model = topic_modelling_type
         self.is_sklearn = is_sklearn
         self.vectorizers = dict()
         self.topic_models = dict()
+        self.show_visualization = show_visualization
+        self.save_fig = save_fig
+        self.num_topics = num_topics
+        self.fig_name = fig_title
+
         # Multiple parameter problem, setting parameters for LDA gensim and TFIDF gensim
-        if type(required_parameters) is dict:
-            self.parameters = required_parameters
+        if type(vectorizer_parameters) is dict:
+            self.parameters = vectorizer_parameters
         else:
             raise CustomException('Type Mismatch: The parameters should be of type dicts')
 
-    def plot_top_words(model, feature_names, n_top_words, title, save_fig):
+        if type(topic_model_parameters) is dict:
+            self.model_parameters = topic_model_parameters
+        else:
+            raise CustomException('Type Mismatch: The parameters should be of type dicts')
+                
+    
+    def plot_top_words(self, model, feature_names, n_top_words, title, save_fig):
         fig, axes = plt.subplots(2, 5, figsize=(30, 15), sharex=True)
         axes = axes.flatten()
         for topic_idx, topic in enumerate(model.components_):
@@ -49,14 +62,15 @@ class Topic_Modelling:
 
     def bow_vectorizer_sklearn(self, corpus, parameters):
         vectorizer = CountVectorizer()
-        try:
-            vectorizer.set_params(parameters)
-        except:
-            raise CustomException('Parameter Error: Check the name and values of parameter for CountVectorizer')
+        if parameters:
+            try:
+                vectorizer.set_params(parameters)
+            except:
+                raise CustomException('Parameter Error: Check the name and values of parameter for CountVectorizer')
         vectorizer.fit(corpus)
         vector = vectorizer.transform(corpus)
         self.vectorizers['BOW_Sklearn'] = vectorizer
-        return vector, self.vectorizers
+        return vector
 
     def vectorizer_gensim(self, corpus, parameters, vectorizer_type):
         corpus_dictionary = Dictionary(corpus)
@@ -66,53 +80,95 @@ class Topic_Modelling:
             tfidf = models.TfidfModel(bow_corpus, id2word=corpus_dictionary)
             tfidf_corpus = tfidf[bow_corpus]
             self.vectorizers['TFIDF_Gensim'] = tfidf_corpus
-            return tfidf_corpus, self.vectorizers
+            return tfidf_corpus
+        elif vectorizer_type == 'bow':
+            return bow_corpus
         else:
-            return bow_corpus, self.vectorizers
+            raise CustomException("Unknow type of vectorizer")
+
 
     def tfidf_vectorizer_sklearn(self, corpus, parameters):
         vectorizer = TfidfVectorizer()
-        try:
-            vectorizer.set_params(parameters)
-        else:
-            raise CustomException('Parameter Error: Check the name and values of parameter for TF-IDF Vectorizer')
+        if parameters:
+            try:
+                vectorizer.set_params(parameters)
+            except:
+                raise CustomException('Parameter Error: Check the name and values of parameter for TF-IDF Vectorizer')
         vectorizer.fit(corpus)
         vector = vectorizer.transform(corpus)
         self.vectorizers['TFIDF_Sklearn'] = vectorizer
-        return vector, self.vectorizers
+        return vector
 
     def tfidf_vectorizer_gensim(self, corpus, parameters):
         corpus_dictionary = Dictionary(corpus)
         bow_corpus = [dictionary.doc2bow(doc) for doc in corpus]
         self.vectorizers['TFIDF_Gensim'] = corpus_dictionary
-        return bow_corpus, self.vectorizers
+        return bow_corpus
 
     def lda_topic_modeling(self, count_features, parameters, feature_names, save_fig, show_visualization = True):
         lda = LatentDirichletAllocation()
-        try:
-            lda.set_params(parameters)
-        raise:
-            CustomException('Parameter Error: Check the name and values of parameter for LatentDirichiletAllocation')
+        if parameters:
+            try:
+                lda.set_params(parameters)
+            except:
+                raise CustomException('Parameter Error: Check the name and values of parameter for LatentDirichiletAllocation')
         lda.fit(count_features)
         self.topic_models['LDA_Sklearn'] = lda
         output = lda.fit_transform(count_features)
         feature_names = feature_names
         if show_visualization:
             # Call the visualizations
-            plot_top_words(lda, feature_names, 10, 'Latent Dirichlet Allocation Topic Model Distribution', save_fig)
-        return output, self.topic_models
+            self.plot_top_words(lda, feature_names, self.num_topics, self.fig_name, save_fig)
+        return output
 
     def nmf_topic_modeling(self, features, parameters, feature_names, save_fig, show_visualization = True):
         nmf = NMF()
-        try:
-            nmf.set_params(parameters)
-        raise:
-            CustomException('Parameter Error: Check the name and values of parameter for Non-Negative Matrix Factorization')
+        if parameters:
+            try:
+                nmf.set_params(parameters)
+            except:
+                raise CustomException('Parameter Error: Check the name and values of parameter for Non-Negative Matrix Factorization')
         nmf.fit(features)
         self.topic_models['NMF_Sklearn'] = nmf
         output = nmf.fit_transform(features)
         feature_names = feature_names
         if show_visualization:
-            plot_top_words(nmf, feature_names, 10, 'Non-negative Matrix Factorization Topic Model Distribution', save_fig)
-        return output, self.topic_models
+            self.plot_top_words(nmf, feature_names, self.num_topics, self.fig_name, save_fig)
+        return output
 
+    def fit_transform(self):
+        corpus = self.data[self.column_name].to_list()
+        print(corpus)
+        if self.is_sklearn:
+            if self.type == "bow":
+                vector = self.bow_vectorizer_sklearn(corpus, self.parameters)
+                feature_names = self.vectorizers['BOW_Sklearn'].get_feature_names()
+                if self.topic_model == "lda":
+                    output = self.lda_topic_modeling(vector, self.model_parameters, feature_names, self.save_fig, show_visualization = self.show_visualization)
+                    return output
+                elif self.topic_model == "nmf":
+                    output = self.nmf_topic_modeling(vector, self.model_parameters, feature_names, self.save_fig, show_visualization = self.show_visualization)
+                    return output
+                else: 
+                    raise CustomException('Unknown type of Topic model')
+            elif self.type == "tfidf":
+                vector = self.tfidf_vectorizer_sklearn(corpus, self.parameters)
+                feature_names = self.vectorizers['TFIDF_Sklearn'].get_feature_names()
+                if self.topic_model == "lda":
+                    raise CustomException('TF-IDF features cannot be used with LDA')
+                elif self.topic_model == "nmf":
+                    output = self.nmf_topic_modeling(vector, self.model_parameters, feature_names, self.save_fig, show_visualization = self.show_visualization)
+                    return output
+                else: 
+                    raise CustomException('Unknown type of Topic model')
+
+            else:
+                raise CustomException("Unknow type of vectorizer")
+        else:
+            vector = self.vectorizer_gensim(corpus, parameters, self.type)
+
+if __name__ == "__main__":
+    import pandas as pd
+    sample_data = pd.read_csv('Sheet_1.csv', nrows = 100)
+    tm = Topic_Modelling(sample_data, 'response_text', vectorizer_type = 'tfidf', topic_modelling_type = 'nmf')
+    tm.fit_transform()
